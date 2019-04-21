39f7ccceb0c82a36c58d9ab02e9d1af2afdcc884
sarahjulesthorne <sarah.jules.thorne@gmail.com>
2019-04-20 23:52:37 -0500

    pushing working domStringBuilder

---
+++ src/javascripts/helpers/dom-string-builder.js
@@ -0,0 +1,34 @@
+import utils from './utils';
+
+const quadDomStringBuilder = (arrayToPrint, divToDisplay) => {
+  let domString = '';
+  arrayToPrint.forEach((quadrant) => {
+    if (quadrant.quadDivId === divToDisplay) {
+      domString += `<div class="quadrant-title-container  ${quadrant.quadDivId}-quadrant-title-container" id="${quadrant.quadDivId}QuadrantTitleContainer">`;
+      domString += `<h3 class="quadrant-title ${quadrant.quadDivId}-quadrant-title" id="${quadrant.quadDivId}QuadrantTitle">${quadrant.quadName}</h3>`;
+      domString += `<p class="quadrant-score ${quadrant.quadDivId}-score" id="${quadrant.scoreDivId}">${quadrant.quadScoreValue}</p>`;
+      domString += '</div>';
+      domString += `<div class="${quadrant.quadDivId}-button-container quadrant-button-container" id="${quadrant.quadDivId}ButtonContainer">`;
+      quadrant.quadrantButtons.forEach((button) => {
+        domString += `<div class="button-container ${quadrant.quadDivId}-button-container" id="${quadrant.buttonId}Container">`;
+        domString += `<button class="${button.buttonClass}" id="${button.buttonId}">${button.buttonName}</button>`;
+        domString += '</div>';
+      });
+      domString += '</div>';
+    }
+  });
+  utils.printToDom(divToDisplay, domString);
+};
+
+const headerDomStringBuilder = () => {
+  let domString = '';
+  domString += '<header>';
+  domString += '<h1>My Tamagotchi</h1>';
+  domString += '</header>';
+  utils.printToDom('app', domString);
+};
+
+export default {
+  quadDomStringBuilder,
+  headerDomStringBuilder,
+};
