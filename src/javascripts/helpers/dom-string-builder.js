4f4b6d3248fd01790b7b6cd1c6d45692c36914e3
sarahjulesthorne <sarah.jules.thorne@gmail.com>
2019-05-06 18:48:25 -0500

    WIP

--- src/javascripts/helpers/dom-string-builder.js
+++ src/javascripts/helpers/dom-string-builder.js
@@ -10,13 +10,13 @@ const quadDomStringBuilder = (arrayToPrint, divToDisplay) => {
   let domString = '';
   arrayToPrint.forEach((quadrant) => {
     if (quadrant.quadDivId === divToDisplay) {
-      domString += `<div class="padding quadrant-title-container  ${quadrant.quadDivId}-quadrant-title-container" id="${quadrant.quadDivId}QuadrantTitleContainer">`;
-      domString += `<h3 class="centered padding quadrant-title ${quadrant.quadDivId}-quadrant-title" id="${quadrant.quadDivId}QuadrantTitle">${quadrant.quadName}</h3>`;
-      domString += `<p class="centered padding quadrant-score ${quadrant.quadDivId}-score" id="${quadrant.scoreDivId}">${quadrant.quadScoreValue}</p>`;
+      domString += `<div class="padding quadrant-title-container" id="${quadrant.quadDivId}QuadrantTitleContainer">`;
+      domString += `<h3 class="centered padding" id="${quadrant.quadDivId}QuadrantTitle">${quadrant.quadName}</h3>`;
+      domString += `<p class="centered padding" id="${quadrant.scoreDivId}">${quadrant.quadScoreValue}</p>`;
       domString += '</div>';
-      domString += `<div class="padding ${quadrant.quadDivId}-button-container quadrant-button-container" id="${quadrant.quadDivId}ButtonContainer">`;
+      domString += `<div class="padding quadrant-button-container" id="${quadrant.quadDivId}ButtonContainer">`;
       quadrant.quadrantButtons.forEach((button) => {
-        domString += `<div class="padding button-container ${quadrant.quadDivId}-button-container" id="${quadrant.buttonId}Container">`;
+        domString += `<div class="padding" id="${quadrant.buttonId}Container">`;
         domString += `<button class="padding ${button.buttonClass}" name="${button.buttonNameAttribute}" id="${button.buttonId}">${button.buttonName}</button>`;
         domString += '</div>';
       });
@@ -31,7 +31,7 @@ passes string into print function which appends header to DOM
 and passes in domString as innerHTML */
 const headerDomStringBuilder = () => {
   let domString = '';
-  domString = '<h1 class="page-title padding centered">My Tamagotchi</h1>';
+  domString = '<h1 class="padding centered">My Tamagotchi</h1>';
   utils.printHeaderToDom('app', domString);
 };
 
